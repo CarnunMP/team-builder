69b6deac0d28dc8e13e53839b24f9041dfaab8ca
CarnunMP <carnun@hotmail.co.uk>
2019-09-18 13:25:53 +0100

    slices of state added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import uuid from "uuid";
 
-function App() {
+const initialTeam = [
+  {name: "Carnun", age: 22, id: uuid()},
+  {name: "Tiy", age: 20, id: uuid()},
+  {name: "Khem", age: 15, id: uuid()},
+  {name: "Rhea", age: 11, id: uuid()},
+];
+
+const initialForm = {
+  name: "",
+  age: "",
+};
+
+export default function App() {
+  const [team, setTeam] = useState(initialTeam);
+  const [form, setForm] = useState(initialForm);
   return (
     <div className="App">
 
     </div>
   );
 }
-
-export default App;
